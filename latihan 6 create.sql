c23e1e7414514c3f5f7f1a1304cb2a3364528d1d
Gardavi nabhan gussasy <gusasig13@gmail.com>
2024-10-11 10:10:47 +0700

    Delete latihan 6 create.sql

--- latihan 6 create.sql
+++
@@ -1,9 +0,0 @@
-create database mahasiswa;
-use mahasiswa;
-CREATE TABLE latihanbasisdataweek6 (
-  NIM CHAR(5) NOT NULL,
-  NAMA VARCHAR(45) NULL,
-  PRODI VARCHAR(45) NULL,
-  BAYAR VARCHAR(45) NULL,
-  PRIMARY KEY (NIM)
-);
